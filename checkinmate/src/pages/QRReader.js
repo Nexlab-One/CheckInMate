1f45209b854ebf84c0da77d36c0f12ec6c3d8430
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2021-09-15 15:41:04 +0930

    Update QRReader.js

--- checkinmate/src/pages/QRReader.js
+++ checkinmate/src/pages/QRReader.js
@@ -3,21 +3,27 @@ import QrReader from "react-qr-reader";
 import { checkin } from "../services/checkin";
 
 function QRReader() {
-  const [result, setResult] = React.useState();
+  const [result, setResult] = React.useState("none");
+  const [scanner, setScanner] = React.useState("block");
   const handleScan = async (data) => {
     if (data) {
-      setResult(data);
-
       const storeID = localStorage.getItem("storeID");
       if (storeID !== "undefined" && storeID !== "") {
         const message = await checkin(data, storeID);
         console.log(message);
         if (message === "Success") {
-          window.location.href = "/checkinsuccess";
+          setResult("block");
+          setScanner("none");
+          // showHideSuccessQR();
+          setTimeout(function () {
+            setResult("none");
+            setScanner("block");
+          }, 2000);
         }
       }
     }
   };
+
   const handleError = (err) => {
     console.error(err);
   };
@@ -37,8 +43,9 @@ function QRReader() {
         </div>
       </section>
       <section className="inner-page">
-        <div className="container">
+        <div className="container" id="qrScanner" style={{ display: scanner }}>
           <h1>QR Scanner Goes here</h1>
+
           <QrReader
             delay={300}
             onError={handleError}
@@ -49,6 +56,16 @@ function QRReader() {
             <a href="/" className="btn btn-primary">
               Check in Another way
             </a>
+          </div>
+        </div>
+        <div class="container">
+          <div
+            id="checkInSuccess"
+            className="alert alert-success row justify-content-center text-center"
+            role="alert"
+            style={{ display: result }}
+          >
+            You have Checked in Successfully!
           </div>
         </div>
       </section>
